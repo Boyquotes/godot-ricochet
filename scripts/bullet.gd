d187d0c2528c445d72c4d705f1f30470f11dab40
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-05-05 17:08:17 +0700

    chore:ammend prev commit

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,26 +1,33 @@
 extends CharacterBody2D
 class_name Bullet
 
-@export var SPEED =20.0
-@export var ricochey = 2 # number of times bulet can ricochet
+@export var SPEED =200
+@export var ricochey = 100 # number of times bulet can ricochet
 @export var damage = 30
 
 var rich_counts = 0:set = _set_rich
+var released = false
+
 func _set_rich(new_rich): 
 	rich_counts = new_rich
 	if rich_counts == ricochey: 
 		$Hitbox.can_dmg = true 
 		
-var veloc = Vector2()
+var veloc = Vector2.ZERO
 func _ready():
-	set_as_top_level(true)
-	
+	if released:
+		set_as_top_level(true)
 
 func _physics_process(delta):
-	var collision = move_and_collide(veloc)
+	_motion(delta)
+	pass 
+	
+func _motion(delta): 
+	var collision = move_and_collide(veloc*delta)
 	if collision: 
 		if  rich_counts < ricochey: 
 			veloc = veloc.bounce(collision.get_normal())
+			look_at(global_position+veloc)
 			rich_counts+=1
 		else: 
 			queue_free()
@@ -29,4 +36,5 @@ func _physics_process(delta):
 func set_veloc(dir:Vector2): 
 	dir = dir.normalized()
 	veloc = dir * SPEED
+	look_at(self.global_position+dir)  
 
