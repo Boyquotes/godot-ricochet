c002d51b256156e1a2e6651db4aa3e45a2f49f59
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-05-05 17:08:47 +0700

    feat(spawner):avoid spawn collision

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -2,11 +2,20 @@ extends StaticBody2D
 
 @export var tier =1
 @export var max_health = 100
+@onready var spawn_zone = $spawn_area/spawn_zone
+@onready var nospawn_zone = $spawn_area/nospawn_zone
+@onready var spawntest= $spawn_test
 
+var spawnable = true
+var spawn_point = null
+var potential_mob = null
 var current_health = 100:set=_set_health
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	spawn_point = generate_spawn_point()
+	pass
+
+	
 
 func _set_health(new_hp): 
 	current_health = new_hp 
@@ -21,13 +30,71 @@ func _process(delta):
 func receive_hit(hitbox:Hitbox): 
 	current_health -= hitbox.damage
 	hitbox.damage_dealt()
+func ran_neg(): 
+	var r = Global.rand_int(2)
+	if r == 0: 
+		return -1 
+	else:
+		return 1
+func generate_spawn_point(): 
+	var x = ran_neg() * Global.rand_float(spawn_zone.shape.radius*2)
+	var y = ran_neg() * Global.rand_float(spawn_zone.shape.radius*2)
 
-func spawn_mob(): 
+	var result=spawn_zone.global_position+Vector2(x,y)
+	
+
+	if nospawn_zone.global_position.distance_to(result) > nospawn_zone.shape.radius:
+		return result
+	else: 
+		return generate_spawn_point()
+
+func move_point_randomly(): 
+	spawntest.global_position = generate_spawn_point() 
+		
+func choose_mob(): 
 	match tier: 
 		1:
 			var mobs = Global.T1_MOBS	
-			var mob = mobs[Global.rand_int(mobs.size())].instantiate()
-			mob.position = $Marker2D.position
-			add_child(mob)
+			var mob =  mobs[Global.rand_int(mobs.size())].instantiate()
+			return mob 			
+			
+func spawn_mob(): 
+	if potential_mob == null: 
+		return 
+	potential_mob.global_position = spawntest.global_position
+	get_tree().get_root().add_child(potential_mob)
+			
+func get_overlaps(): 
+	return get_tree().get_nodes_in_group(self.name+ " overlapped")
+
+func _physics_process(delta):
+	if spawntest.get_overlapping_bodies().size()==0:	
+		spawnable = true
+	else: 
+		spawnable=false
+		move_point_randomly() 
+		
 func _on_spawn_rate_timeout():
-	spawn_mob()
+	if spawnable: 
+		potential_mob = choose_mob()
+		spawntest.get_node("CollisionShape2D").shape = potential_mob.get_node("CollisionShape2D").shape
+		spawn_mob()
+		spawnable=false
+		move_point_randomly()
+
+#func _on_spawn_test_body_entered(body):
+#	if body == null: 
+#		return 
+#	body.add_to_group(self.name+" overlapped")
+#
+#func _on_spawn_test_body_exited(body):
+#	if body == null: 
+#		return
+#	body.remove_from_group(self.name+" overlapped")
+#
+#	if get_tree().get_nodes_in_group(self.name+ " overlapped").size() == 0:
+#		spawn_mob()
+#		spawnable = true
+#
+#	move_point_randomly()
+#	pass # Replace with function body.
